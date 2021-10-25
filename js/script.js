dd12c111fe98e03d8f2764191367f68585e389d3
Bonnni <60107357+Bonnni@users.noreply.github.com>
2021-10-25 17:40:44 +0500

    update files

--- js/script.js
+++ js/script.js
@@ -12,23 +12,3 @@ for (let anchor of anchors) {
         })
     })
 }
-
-ymaps.ready(init);
-
-function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [59.612151, 60.574244],
-        zoom: 7,
-        controls: ['default']
-    });
-
-    var placemark = new ymaps.Placemark([59.612151, 60.574244], {}, {
-        // Задаем стиль метки (метка в виде круга).
-        preset: "islands#circleDotIcon",
-        // Задаем цвет метки (в формате RGB).
-        iconColor: '#ff0000'
-    });
-
-    myMap.geoObjects.add(placemark);
-
-};
